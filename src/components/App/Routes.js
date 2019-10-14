d2f0e8bbc567f0e8d406f3fdf19bc0626040ed4a
Jesse Dunlap <jesse@hydrais.com>
2019-10-14 16:43:21 -0500

    Fixed a few bugs

--- src/components/App/Routes.js
+++ src/components/App/Routes.js
@@ -2,18 +2,19 @@ import React from "react";
 import { Switch, Route, Redirect } from "react-router-dom";
 import CollectionsPage from "../../pages/Database/Collections/CollectionsPage";
 import UsersPage from "../../pages/Auth/Users/UsersPage";
-import EventsPage from "../../pages/Analytics/Events/EventsPage";
 
 function Routes() {
   return (
     <Switch>
-      <Route exact path="/database" render={() => <Redirect to="/database/collections" />} />
+      <Route
+        exact
+        path="/database"
+        render={() => <Redirect to="/database/collections" />}
+      />
       <Route path="/database/collections" component={CollectionsPage} />
 
       <Route exact path="/auth" render={() => <Redirect to="/auth/users" />} />
       <Route path="/auth/users" component={UsersPage} />
-
-      <Route path="/analytics/events" component={EventsPage} />
     </Switch>
   );
 }
